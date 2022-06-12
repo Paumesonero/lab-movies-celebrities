3214089290cf0233a23b1ec28226ffca457d2327
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-06-12 21:42:16 +0200

    iterations 1-6 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,11 +1,11 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 const { route } = require("express/lib/application");
-const async = require("hbs/lib/async");
 const Celebrities = require('../models/Celebrity.model')
 
 // all your routes here
 
+// Iteration 3
 router.get('/create', (req, res, next) => {
     res.render('celebrities/new-celebrity')
 })
@@ -13,8 +13,19 @@ router.get('/create', (req, res, next) => {
 router.post('/create', async (req, res, next) => {
     const { name, occupation, catchPhrase } = req.body
     try {
-        await Celebrities.create({ name, occupation, catchPhrase })
-        res.redirect('/celebrities')
+        await Celebrities.create({ name, occupation, catchPhrase });
+        res.redirect("/celebrities");
+    } catch (error) {
+        res.redirect('/celebrities/create');
+    }
+})
+
+// Iteration 4
+
+router.get('/', async (req, res, next) => {
+    try {
+        const celebrities = await Celebrities.find({});
+        res.render('celebrities/celebrities-view', { celebrities })
     } catch (error) {
         next(error)
     }
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,45 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movies = require("../models/Movie.model")
+const Celebrities = require('../models/Celebrity.model');
+const async = require("hbs/lib/async");
+const res = require("express/lib/response");
+const req = require("express/lib/request");
 
 // all your routes here
+router.get('/create', async (req, res, next) => {
+    try {
+        const celebrities = await Celebrities.find({})
+        res.render('movies/new-movie', { celebrities })
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.post('/create', async (req, res, next) => {
+    const { title, genre, plot, cast } = req.body
+    try {
+        await Movies.create({ title, genre, plot, cast });
+        res.redirect("/movies");
+    } catch (error) {
+        res.redirect('/movies/create');
+    }
+})
+
+//iteration 7
+
+router.get('/', async (req, res, next) => {
+    try {
+        const movies = await Movies.find({});
+        res.render('movies/movies-view', { movies })
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+
+
 
 module.exports = router;
